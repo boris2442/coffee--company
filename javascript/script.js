a54832b370274333cdd07b1ad994d903aaa17332
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-12 09:27:08 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,5 @@
-const navig=document.querySelector("header ul ")
+// const navig=document.querySelector("header  ul ");
+// const menu=document.querySelector(".burger");
+// menu.addEventListener("click", ()=>{
+//     navig.classList.toggle("navigation")
+// })
