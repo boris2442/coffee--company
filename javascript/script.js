943f857eda236916353a10182a3a2dd63345e157
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-26 07:41:21 +0100

    text

--- javascript/script.js
+++ javascript/script.js
@@ -37,3 +37,33 @@ typewriter
 .pauseFor(2000)
 .deleteAll("")
 .start();
+
+
+// partie swiper
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    delay:2500,
+    autoplay:{
+        disableOnInteraction: false, 
+    },
+  speed:500,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
